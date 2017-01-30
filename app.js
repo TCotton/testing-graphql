7927d0fa790cb0ba74a0934382c0b512b25da6c3
Andy Walpole <me@andywalpole.me>
2017-01-30 22:44:11 +0000

    added all files

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http');
+const graphql = require('graphql').graphql;
+const graphQLHTTP = require('express-graphql');
+const actionsSchema = require('./schema/actions');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+const query = 'query { actions { chanId } }';
+
+graphql(actionsSchema, query).then(function(result) {
+  console.dir(result);
+});
+
+app.use('/actions', graphQLHTTP({
+  schema: actionsSchema,
+  pretty: true,
+  graphiql: true
+}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = http.createServer(app);
+
+module.exports = server.listen(8080, (err) => {
+
+  if (err) {
+    return console.log('server error: ', err);
+  }
+
+  return console.log(`server is listening on 8080`);
+
+});
+
+module.exports = app;
---
+++ schema/actions.js
@@ -0,0 +1,131 @@
+const graphql = require('graphql');
+const https = require('https');
+const generateSchema = require('json-to-graphql');
+const fs = require('fs');
+
+function returnDATA() {
+
+  return new Promise((resolve, reject) => {
+
+    return https.get({
+      host: 'actioncyspecsearch.firebaseio.com',
+      path: '/actions.json'
+    }, (response) => {
+
+      let body = '';
+
+      response.on('data', function(data) {
+        body += data;
+      });
+
+      response.on('end', function() {
+        resolve(JSON.parse(body));
+      });
+
+    }).on('error', function(e) {
+      reject('problem with request: ' + e.message);
+    });
+
+  });
+
+}
+
+const TODOs = {
+  "actionId1": {
+    "chanId": "facebook",
+    "descr": "Lorem ipsum",
+    "howToIds": [
+      null,
+      "howToId1",
+      "howToId3"
+    ],
+    "imgUrl": "http://placehold.it/300x300?text=ActionScreenshot",
+    "name": "Page Post Photo",
+    "reqIds": [
+      null,
+      "reqId1",
+      "reqId4"
+    ],
+    "url": "page-post-photo"
+  },
+  "actionId2": {
+    "chanId": "twitter",
+    "descr": "Lorem ipsum tweet tweet",
+    "howToIds": [
+      null,
+      "howToId2"
+    ],
+    "imgUrl": "http://placehold.it/300x300?text=ActionScreenshot",
+    "name": "Tweet",
+    "reqIds": [
+      null,
+      "reqId2"
+    ],
+    "url": "tweet"
+  },
+  "actionId3": {
+    "chanId": "facebook",
+    "descr": "Lorem ipsum",
+    "howToIds": [
+      null,
+      "howToId4"
+    ],
+    "imgUrl": "http://placehold.it/300x300?text=ActionScreenshot",
+    "name": "Upload Video",
+    "reqIds": [
+      null,
+      "reqId3"
+    ],
+    "url": "upload-video"
+  }
+};
+
+/*returnDATA().then(function(returnedData) {
+
+
+  fs.writeFile('schema.js', schema, function(result) {
+    console.dir(result);
+  });
+
+});*/
+
+const ActionsType = new graphql.GraphQLObjectType({
+  name: 'actions',
+  fields: function() {
+    return {
+      chanId: {
+        type: graphql.GraphQLString
+      },
+      descr: {
+        type: graphql.GraphQLString
+      },
+      imgUrl: {
+        type: graphql.GraphQLString
+      },
+      name: {
+        type: graphql.GraphQLString
+      },
+      url: {
+        type: graphql.GraphQLString
+      }
+    }
+  }
+});
+
+const queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: function() {
+    return {
+      actions: {
+        type: new graphql.GraphQLList(ActionsType),
+        resolve: function () {
+          return TODOs;
+        }
+      }
+    }
+  }
+});
+
+module.exports = new graphql.GraphQLSchema({
+  query: queryType
+});
